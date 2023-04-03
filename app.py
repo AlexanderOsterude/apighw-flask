0399fad8be88d083544351356cbd47cf8cb11ae6
unknown <alexosterude@gmail.com>
2023-04-03 15:59:52 -0400

    we forgot to return anything

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def hello_ghw():
 def getHackathons():
     if request.method == "POST":
         hackathons_list["New Hackathon"] = request.json
+        return hackathons_list
     else:
         return hackathons_list
 
